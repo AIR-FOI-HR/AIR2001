456a1e3d2729ba6e72041dc48f10a9c6da63abe0
DESKTOP-TPTQ5AJ\User <krasinec20@foi.hr>
2020-11-05 22:21:57 +0100

    Dodani Date picker i stavljene varijable u View Model, te dodao i elemente za kasniju implementaciju Time pickera

--- app/src/main/java/com/example/beervana/AddEventActivity.java
+++ app/src/main/java/com/example/beervana/AddEventActivity.java
@@ -1,6 +1,79 @@
 package com.example.beervana;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.beervana.databinding.ActivityAddeventBinding;
+
+import java.util.Calendar;
+
+
 public class AddEventActivity extends AppCompatActivity {
+    private ActivityAddeventBinding binding;
+    private ImageView slikaDogadjaj;
+    private EditText unosImedogadjaja;
+    private EditText unosOpisaDogadaja;
+    private TextView errSlika;
+    private TextView errUnosImeDogadjaja;
+    private TextView errOpisaDogadjaja;
+    private TextView errUnosDatum;
+    private TextView errUnosVrijeme;
+    private TextView prikazDatuma;
+    private TextView prikazVremena;
+
+    private DatePickerDialog.OnDateSetListener listenerZaDatum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAddeventBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        prikazDatuma = binding.prikazDatuma;
+        prikazVremena = binding.prikazVremena;
+        slikaDogadjaj = binding.slikaDogadjaj;
+        unosImedogadjaja = binding.unosImeDogadaja;
+        unosOpisaDogadaja = binding.unosOpisDogadaja;
+        errSlika = binding.errSlika;
+        errUnosImeDogadjaja = binding.errUnosImeDogadaja;
+        errOpisaDogadjaja = binding.errUnosOpisdogadaja;
+        errUnosDatum = binding.errUnosDatum;
+        errUnosVrijeme = binding.errUnosVrijeme;
+
+
+        binding.btnOdaberiDatum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int godina = calendar.get(Calendar.YEAR);
+                int mjesec = calendar.get(Calendar.MONTH);
+                int dan = calendar.get(calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog
+                        (AddEventActivity.this,R.style.Theme_AppCompat_Light_Dialog_MinWidth,
+                                listenerZaDatum,godina,mjesec,dan);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.LTGRAY));
+                dialog.show();
+            }
+        });
+        listenerZaDatum = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                prikazDatuma.setText(dayOfMonth +"/"+month+"/"+year);
+            }
+        };
+
+    }
+
+
 }
--- app/src/main/java/com/example/beervana/AddEventActivityViewModel.java
+++ app/src/main/java/com/example/beervana/AddEventActivityViewModel.java
@@ -1,4 +1,110 @@
 package com.example.beervana;
 
+import android.net.Uri;
+import android.view.View;
+
+
 public class AddEventActivityViewModel {
+    private Uri slika;
+    private String unosImedogadjaja;
+    private String unosOpisaDogadaja;
+    private String prikazDatuma;
+    private String prikazVremena;
+    private String errSlika;
+    private String errUnosImeDogadjaja;
+    private String errOpisaDogadjaja;
+    private String errUnosDatum;
+    private String errUnosVrijeme;
+
+    private int errSlikaVisibility;
+    private int errUnosImeDogadjajaVisibility;
+    private int errOpisaDogadjajaVisibility;
+    private int errUnosDatumVisibility;
+    private int errUnosVrijemeVisibility;
+
+    int gone = View.GONE;
+    int visible=View.VISIBLE;
+
+    public Uri getSlika() {
+        return slika;
+    }
+
+    public void setSlika(Uri slika) {
+        this.slika = slika;
+    }
+
+    public String getUnosImedogadjaja() {
+        return unosImedogadjaja;
+    }
+
+    public void setUnosImedogadjaja(String unosImedogadjaja) {
+        this.unosImedogadjaja = unosImedogadjaja;
+    }
+
+    public String getUnosOpisaDogadaja() {
+        return unosOpisaDogadaja;
+    }
+
+    public void setUnosOpisaDogadaja(String unosOpisaDogadaja) {
+        this.unosOpisaDogadaja = unosOpisaDogadaja;
+    }
+
+    public String getPrikazDatuma() {
+        return prikazDatuma;
+    }
+
+    public void setPrikazDatuma(String prikazDatuma) {
+        this.prikazDatuma = prikazDatuma;
+    }
+
+    public String getPrikazVremena() {
+        return prikazVremena;
+    }
+
+    public void setPrikazVremena(String prikazVremena) {
+        this.prikazVremena = prikazVremena;
+    }
+
+    public String getErrSlika() {
+        return errSlika;
+    }
+
+    public void setErrSlika(String errSlika) {
+        this.errSlika = errSlika;
+    }
+
+    public String getErrUnosImeDogadjaja() {
+        return errUnosImeDogadjaja;
+    }
+
+    public void setErrUnosImeDogadjaja(String errUnosImeDogadjaja) {
+        this.errUnosImeDogadjaja = errUnosImeDogadjaja;
+    }
+
+    public String getErrOpisaDogadjaja() {
+        return errOpisaDogadjaja;
+    }
+
+    public void setErrOpisaDogadjaja(String errOpisaDogadjaja) {
+        this.errOpisaDogadjaja = errOpisaDogadjaja;
+    }
+
+    public String getErrUnosDatum() {
+        return errUnosDatum;
+    }
+
+    public void setErrUnosDatum(String errUnosDatum) {
+        this.errUnosDatum = errUnosDatum;
+    }
+
+    public String getErrUnosVrijeme() {
+        return errUnosVrijeme;
+    }
+
+    public void setErrUnosVrijeme(String errUnosVrijeme) {
+        this.errUnosVrijeme = errUnosVrijeme;
+    }
+
+
+
 }
