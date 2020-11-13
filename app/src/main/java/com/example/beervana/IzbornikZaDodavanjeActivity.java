14f1a76b723670cef3334efb594970308bbfe7d5
jantolos <jantolos@foi.hr>
2020-11-13 16:44:14 +0100

    Ikone u aplikaciji i izbornik za dodavanje na kojem potrebne male izmjene.

---
+++ app/src/main/java/com/example/beervana/IzbornikZaDodavanjeActivity.java
@@ -0,0 +1,13 @@
+package com.example.beervana;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IzbornikZaDodavanjeActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_izbornik_za_dodavanje);
+    }
+
+}
