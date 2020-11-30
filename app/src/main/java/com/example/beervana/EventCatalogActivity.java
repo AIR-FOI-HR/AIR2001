d8f3cc6267f3676d19e32d7f3f55592b9c23bf59
DESKTOP-TPTQ5AJ\User <krasinec20@foi.hr>
2020-11-30 18:56:33 +0100

    Dodao brisanje eventova.

--- app/src/main/java/com/example/beervana/EventCatalogActivity.java
+++ app/src/main/java/com/example/beervana/EventCatalogActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,17 +15,22 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.example.webservice.DohvatPodataka;
+import com.example.webservice.SlanjePodataka;
 
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EventCatalogActivity extends AppCompatActivity {
     ListView listView;
     private RequestQueue requestQueue;
+    private RequestQueue requestQueueBrisanje;
     EventCatalogAdapter adapter;
     public static ArrayList<ModelPodatakEventCatalog> eventDataList = new ArrayList<>();
     String url = "https://beervana2020.000webhostapp.com/test/DohvacanjeDogadaja.php";
+    String urlBrisanje = "https://beervana2020.000webhostapp.com/test/ObrisiDogadaj.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +57,7 @@ public class EventCatalogActivity extends AppCompatActivity {
                             case 1:
                                 break;
                             case 2:
+                                DeleteEvent(position);
                                 break;
                         }
                     }
@@ -76,6 +83,27 @@ public class EventCatalogActivity extends AppCompatActivity {
                     eventDataList.addAll(logikaEventCatalog.parsiranjePodatakaEventData(odgovor));
                     adapter.notifyDataSetChanged();
                 }
+            }
+        });
+
+    }
+    private void DeleteEvent(int pozicija){
+        requestQueueBrisanje = Volley.newRequestQueue(getApplicationContext());
+        SlanjePodataka slanjePodataka = new SlanjePodataka(urlBrisanje);
+        Map<String, String> params=new HashMap<String, String>();
+        params.put("id_dogadaja", EventCatalogActivity.eventDataList.get(pozicija).dogadaj.getIdDogadaj());
+        slanjePodataka.setParametri(params);
+        slanjePodataka.sendData(getApplicationContext(),requestQueueBrisanje);
+        requestQueueBrisanje.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<Object>() {
+            @Override
+            public void onRequestFinished(Request<Object> request){
+                String odgovor = slanjePodataka.getOdgovor();
+                if (odgovor.equals("Successfully deleted an event")){
+                    eventDataList.remove(pozicija);
+                    adapter.notifyDataSetChanged();
+                }
+                Toast toast = Toast.makeText(getApplicationContext(),odgovor,Toast.LENGTH_LONG);
+                toast.show();
             }
         });
 
