d5fa66cb76d86ae86eef64ed451f26cfa8ae8b4d
jantolos <jantolos@foi.hr>
2020-11-30 09:24:13 +0100

    Prikaz podataka o pivovari.

---
+++ app/src/main/java/com/example/beervana/PrikazPodatakaOPivovariActivity.java
@@ -0,0 +1,17 @@
+package com.example.beervana;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class PrikazPodatakaOPivovariActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prikaz_podataka_o_pivovari);
+    }
+}
+
