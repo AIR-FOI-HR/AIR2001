a52d54d9da19d05d887279cdb8c7a2cbe59a765d
DESKTOP-TPTQ5AJ\User <krasinec20@foi.hr>
2020-11-30 16:25:57 +0100

    Napravljeni elementi u layoutu, te spojeno stavljanje podataka na layout.

--- app/src/main/java/com/example/beervana/PrikazZaEventPodatkeActivity.java
+++ app/src/main/java/com/example/beervana/PrikazZaEventPodatkeActivity.java
@@ -1,16 +1,41 @@
 package com.example.beervana;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.squareup.picasso.Picasso;
+
 public class PrikazZaEventPodatkeActivity  extends AppCompatActivity {
+    ImageView slikaDogadaja;
+    TextView nazivDogadaja,nazivLokacije,opisDogadaja,datumOd,datumDo;
+    int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prikaz_za_event_podatke);
 
+        nazivDogadaja = findViewById(R.id.prikazNazivDogadaja);
+        nazivLokacije = findViewById(R.id.prikazNazivLokacije);
+        opisDogadaja = findViewById(R.id.prikazOpisDogadaja);
+        datumOd = findViewById(R.id.prikazDatumOd);
+        datumDo = findViewById(R.id.prikazDatumDo);
+        slikaDogadaja = findViewById(R.id.prikazSlikaDogadjaj);
 
+        Intent intent = getIntent();
+        position = intent.getExtras().getInt("position");
+
+        nazivDogadaja.setText(EventCatalogActivity.eventDataList.get(position).dogadaj.getNazivDogadaj());
+        nazivLokacije.setText(EventCatalogActivity.eventDataList.get(position).getNazivLokacije());
+        opisDogadaja.setText(EventCatalogActivity.eventDataList.get(position).dogadaj.getOpisDogadaja());
+        datumOd.setText("Start: " + EventCatalogActivity.eventDataList.get(position).dogadaj.getDatumOd());
+        datumDo.setText("End: "+ EventCatalogActivity.eventDataList.get(position).dogadaj.getDatumDo());
+
+        String imageUri = EventCatalogActivity.eventDataList.get(position).dogadaj.getSlikaDogadaja();
+        Picasso.with(this).load(imageUri).into(slikaDogadaja);
     }
 }
