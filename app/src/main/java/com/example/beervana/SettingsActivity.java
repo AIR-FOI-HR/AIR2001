1b0f6a5e5cc39da3e845f00b36e08515214e502a
edujak <edujak@foi.hr>
2020-12-05 14:36:28 +0100

    Kreirani layout za postavke zajedno s aktivnosti.

---
+++ app/src/main/java/com/example/beervana/SettingsActivity.java
@@ -0,0 +1,29 @@
+package com.example.beervana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Button button = (Button) findViewById(R.id.buttonAboutUs);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2(){
+        //Intent intent = new Intent(this, AboutUsActivity.class);
+        //startActivity(intent);
+    }
+}
