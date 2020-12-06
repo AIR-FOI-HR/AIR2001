cb5dc0129168f357dfccbf157bbc830787c25076
edujak <edujak@foi.hr>
2020-12-06 23:15:38 +0100

    Kreirana funkcionalna alatna traka za aplikaciju.

--- app/src/main/java/com/example/beervana/SettingsActivity.java
+++ app/src/main/java/com/example/beervana/SettingsActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.widget.Toolbar;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -13,6 +17,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
         Button button = (Button) findViewById(R.id.buttonAboutUs);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,10 +25,48 @@ public class SettingsActivity extends AppCompatActivity {
                 openActivity2();
             }
         });
+
+        //OVAJ DIO DODATI ZA TOOLBAR
+        ImageView mImageView = (ImageView)findViewById(R.id.settings_icon);
+        mImageView.setOnClickListener(v -> openActivity3());
+
+        //OVAJ DIO OTKOMENTIRATI KAD SE KREIRAJU AKTIVNOSTI
+        /*
+        ImageView mImageView = (ImageView)findViewById(R.id.user_icon);
+        mImageView.setOnClickListener(v -> openActivity4());
+
+        ImageView mImageView = (ImageView)findViewById(R.id.search_icon);
+        mImageView.setOnClickListener(v -> openActivity5());
+        */
+
+        //
+
+
     }
 
     public void openActivity2(){
         //Intent intent = new Intent(this, AboutUsActivity.class);
         //startActivity(intent);
     }
+
+    //I OVAJ DIO DODATI ZA TOOLBAR
+    public void openActivity3(){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    //OVAJ DIO OTKOMENTIRATI KAD SE KREIRAJU AKTIVNOSTI
+    /*
+    public void openActivity4(){
+        Intent intent = new Intent(this, UserActivity.class);
+        startActivity(intent);
+    }
+    public void openActivity5(){
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }
+     */
+
+    //KRAJ
+
 }
