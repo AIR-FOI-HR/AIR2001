b09c0b5ac0a3cb13e0acb078db3377b094c3533d
jantolos <jantolos@foi.hr>
2020-12-20 16:44:32 +0100

    Popravak logout-a. Da se brise sesija.

--- app/src/main/java/com/example/beervana/SettingsActivity.java
+++ app/src/main/java/com/example/beervana/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.beervana;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +34,12 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
+
+                SharedPreferences preferences =getSharedPreferences("loginPrefs",MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.clear();
+                editor.apply();
+                finish();
             }
         });
         //OVAJ DIO DODATI ZA TOOLBAR
