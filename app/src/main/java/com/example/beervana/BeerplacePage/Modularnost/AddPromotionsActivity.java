955ef28eb6fdfaba6953ce85971f45efedd4acf9
Babovic-Tomislav <tbabovic20@foi.hr>
2021-01-14 16:10:36 +0100

    RecylcerView i mock moduli napravljeni.

--- app/src/main/java/com/example/beervana/BeerplacePage/Modularnost/AddPromotionsActivity.java
+++ app/src/main/java/com/example/beervana/BeerplacePage/Modularnost/AddPromotionsActivity.java
@@ -3,22 +3,34 @@ package com.example.beervana.BeerplacePage.Modularnost;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.beervana.BaseActivity;
 import com.example.beervana.MainActivity;
 import com.example.beervana.R;
 
-public class AddPromotions extends BaseActivity {
+public class AddPromotions extends BaseActivity implements AddPromotionsRecylcerAdapter.onAddPromosListener{
+    RecyclerView recyclerView;
+    AddPromotionsRecylcerAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beerplace_homepage_new);
         initToolbar();
-        MainActivity m = new MainActivity();
 
-        Intent intent = new Intent();
-        m.startActivity(intent);
-        startActivity(intent);
 
+
+
+    }
+
+    @Override
+    public void onAddPromosClick(int position) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
---
+++ app/src/main/java/com/example/beervana/BeerplacePage/Modularnost/BaseClassForModules.java
@@ -0,0 +1,4 @@
+package com.example.beervana.BeerplacePage.Modularnost;
+
+public class BaseClassForModule {
+}
